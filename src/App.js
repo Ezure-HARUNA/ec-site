86546e509a710bb3341a2745bbb5db8332e593e7
ezure haruna <burimofumofu@gmail.com>
2021-12-16 13:52:09 +0900

    add:App.jsのページ遷移実装

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import "./App.css";
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import './App.css';
 import HomePage from "./components/pages/homepage/HomePage";
 
-const App = () => {
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE </h1>
+  </div>
+);
+
+function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+      <Router>
+        <div>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route  exact path='/hats' component={HatsPage} />
+          </Switch>
+        </div>
+      </Router>
   );
-};
+}
 
 export default App;
