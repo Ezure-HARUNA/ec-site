7cbdde955f29aa39907c3af902f1f9ab5808b884
ezure haruna <burimofumofu@gmail.com>
2021-12-17 15:06:14 +0900

    add:Shopページに画像追加

--- src/components/CollectionPreview/CollectionPreview.jsx
+++ src/components/CollectionPreview/CollectionPreview.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-import "./CollectionPreview.styles.css";
+import CollectionItem from "../CollectionItem/CollectionItem";
+import "./CollectionPreview.styles.scss";
 
 const PreviewCollection = ({ title, items }) => {
   return (
     <div className="collection-preview">
       <h1 className="title">{title.toUpperCase()}</h1>
       <div className="preview">
-        {items.map((item) => (
-          <div key={item.id}>{item.name}</div>
+        {items.map(({ id, ...otherItemProps }) => (
+          <CollectionItem key={id} {...otherItemProps}></CollectionItem>
         ))}
       </div>
     </div>
