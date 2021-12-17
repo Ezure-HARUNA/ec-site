8e1163a245a962ebc20d6a6a49091d646ba48e81
ezure haruna <burimofumofu@gmail.com>
2021-12-17 13:49:35 +0900

    add:Shopコンポーネントの追加・React Router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import "./App.css";
 import HomePage from "./components/pages/homepage/HomePage";
-import HatsPage from "./components/pages/hatspage/HatsPage"
-import SneakersPage from "./components/pages/sneakerspage/SneakersPage"
-import JacketsPage from "./components/pages/jacketspage/JacketsPage"
-import MensPage from "./components/pages/menspage/MensPage"
-import WomensPage from "./components/pages/womenspage/WomensPage"
-
+import ShopPage from "./components/pages/shop/Shop";
+import HatsPage from "./components/pages/hatspage/HatsPage";
+import SneakersPage from "./components/pages/sneakerspage/SneakersPage";
+import JacketsPage from "./components/pages/jacketspage/JacketsPage";
+import MensPage from "./components/pages/menspage/MensPage";
+import WomensPage from "./components/pages/womenspage/WomensPage";
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Switch>
-            <Route exact path='/' component={HomePage} />
-            <Route  exact path='/hats' component={HatsPage} />
-            <Route  exact path='/jackets' component={JacketsPage} />
-            <Route  exact path='/sneakers' component={SneakersPage} />
-            <Route  exact path='/mens' component={MensPage} />
-            <Route  exact path='/womens' component={WomensPage} />
-          </Switch>
-        </div>
-      </Router>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/hats" component={HatsPage} />
+          <Route exact path="/jackets" component={JacketsPage} />
+          <Route exact path="/sneakers" component={SneakersPage} />
+          <Route exact path="/mens" component={MensPage} />
+          <Route exact path="/womens" component={WomensPage} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
