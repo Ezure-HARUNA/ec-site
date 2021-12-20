7ce0c4bfe7946a59cd068a18fcd5efa6fa7345c3
ezure haruna <burimofumofu@gmail.com>
2021-12-20 19:59:59 +0900

    fix:shopデータの移動

--- src/components/collection-overview/CollectionOverview.jsx
+++ src/components/collection-overview/CollectionOverview.jsx
@@ -3,20 +3,19 @@ import { connect } from "react-redux";
 import "./CollectionOverview.styles.scss";
 import { createStructuredSelector } from "reselect";
 import CollectionPreview from "../../components/CollectionPreview/CollectionPreview";
+import { selectCollectionsForPreview } from "../../redux/shop/shop.selectors";
 import { selectCollections } from "../../redux/shop/shop.selectors";
 
-const CollectionOverview = ({ collections }) => (
+const CollectionsOverview = ({ collections }) => (
   <div className="collections-overview">
-    {collections
-      .filter((item, idx) => idx < 4)
-      .map(({ id, ...otherCollectionProps }) => (
-        <CollectionPreview key={id} {...otherCollectionProps} />
-      ))}
+    {collections.map(({ id, ...otherCollectionProps }) => (
+      <CollectionPreview key={id} {...otherCollectionProps} />
+    ))}
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
-  collections: selectCollections,
+  collections: selectCollectionsForPreview,
 });
 
-export default connect(mapStateToProps)(CollectionOverview);
+export default connect(mapStateToProps)(CollectionsOverview);
--- src/components/pages/collection/Collection.jsx
+++ src/components/pages/collection/Collection.jsx
@@ -1,20 +1,19 @@
 import React from "react";
 import { connect } from "react-redux";
-import CollectionItem from "../../cart-item/CartItem";
+import CollectionItem from "../../CollectionItem/CollectionItem";
 import { selectCollection } from "../../../redux/shop/shop.selectors";
 import "./Collection.styles.scss";
 
 const Collection = ({ collection }) => {
-  // const { title, items } = collection;
-  console.log(collection);
+  const { title, items } = collection;
   return (
     <div className="collection-page">
-      <h2 className="title">collection page</h2>
-      {/* <div className="items">
+      <h2 className="title">{title}</h2>
+      <div className="it">
         {items.map((item) => (
           <CollectionItem key={item.id} item={item} />
         ))}
-      </div> */}
+      </div>
     </div>
   );
 };
